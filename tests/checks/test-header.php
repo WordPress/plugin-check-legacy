386e4f5795ce34ad4252e775906536479fc1a450
Dion Hulse <contact@dd32.id.au>
2023-05-11 15:17:06 +1000

    Add coverage for checks/header.php

---
+++ tests/checks/test-header.php
@@ -0,0 +1,18 @@
+<?php
+
+class Test_Header extends PluginCheck_TestCase {
+	public function test_textdomain() {
+		$usage = '
+		/*
+		 * Plugin Name: Test Plugin
+		 * Text Domain: example-plugin
+		 */
+		';
+
+		$matching_slug = $this->run_against_string( $usage, [ 'slug' => 'example-plugin' ] );
+		$mismatch_slug = $this->run_against_string( $usage, [ 'slug' => 'mismatched-slug' ] );
+
+		$this->assertHasErrorType( $mismatch_slug, [ 'type' => 'warning', 'code' => 'textdomain_mismatch', 'needle' => 'mismatched-slug' ] );
+		$this->assertNotHasErrorType( $matching_slug, [ 'type' => 'warning', 'code' => 'textdomain_mismatch', 'needle' => 'example-plugin' ] );
+	}
+}
