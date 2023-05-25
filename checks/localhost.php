4bc63720fef6981aee2b033bf2124d54ef3430a7
Dion Hulse <contact@dd32.id.au>
2023-05-25 11:40:05 +1000

    Check: Switch to using a singular regex for the matching, this means it only needs to loop over the files once. It also allows us to extract the match and include that in the error message.

--- checks/localhost.php
+++ checks/localhost.php
@@ -5,15 +5,14 @@ use WordPressdotorg\Plugin_Check\{Error, Guideline_Violation, Message, Notice, W
 class Localhost extends Check_Base {
 
 	function check_localhost() {
-		if (
-			$this->scan_matching_files_for_needle( 'http://localhost', '\.php$' ) ||
-			$this->scan_matching_files_for_needle( 'https://localhost', '\.php$' ) ||
-			$this->scan_matching_files_for_needle( 'https://127.0.0.1', '\.php$' ) ||
-				$this->scan_matching_files_for_needle( 'http://127.0.0.1', '\.php$' )
-		) {
+		$found = $this->scan_matching_files_for_needle( '#https?://(localhost|127.0.0.1)#', '\.php$' );
+		if ( $found ) {
 			return new Error(
 				'localhost_code_detected',
-				__( 'Do not use Localhost in your code. Detected: localhost', 'plugin-check' )
+				sprintf(
+					__( 'Do not use Localhost in your code. Detected: %s', 'plugin-check' ),
+					$found
+				)
 			);
 		}
 	}
