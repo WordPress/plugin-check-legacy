b77c49613b17f9100048e38a40c24e3a3c11bc40
Evan Herman <evan.m.herman@gmail.com>
2023-09-01 10:03:47 -0400

    Update logic

--- checks/check-constants.php
+++ checks/check-constants.php
@@ -4,15 +4,19 @@ use WordPressdotorg\Plugin_Check\{Error, Guideline_Violation, Message, Notice, W
 
 class Code_Constants extends Check_Base {
 
-	function check_allow_unfiltered_uploads() {
-		if (
-			$this->scan_matching_files_for_needle( 'ALLOW_UNFILTERED_UPLOADS', '\.php$' )
-		) {
-			return new Error(
-				'allow_unfiltered_uploads_detected',
-				__( 'ALLOW_UNFILTERED_UPLOADS is not permitted.', 'wporg-plugins' )
-			);
+	public function check_allow_unfiltered_uploads() {
+
+		if ( ! $this->scan_matching_files_for_needle( 'ALLOW_UNFILTERED_UPLOADS', '\.php$' ) ) {
+
+			return;
+
 		}
+
+		return new Error(
+			'allow_unfiltered_uploads_detected',
+			__( 'ALLOW_UNFILTERED_UPLOADS is not permitted.', 'wporg-plugins' )
+		);
+
 	}
 
 }
